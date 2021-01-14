a0028bacec7e0ec96307794ee4eb7d48e124da23
Raffaello Bonghi <raffaello@rnext.it>
2021-01-14 23:45:12 +0100

    Change build

--- build.sh
+++ build.sh
@@ -49,6 +49,7 @@ fi
 echo $(pwd) $ROS_DISTRO
 # Build 
 cd jetson-containers
-echo "sh ./scripts/docker_build.sh ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION Dockerfile.ros.$ROS_DISTRO --build-arg BASE_IMAGE=$BASE_IMAGE"
+#echo "sh ./scripts/docker_build.sh ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION Dockerfile.ros.$ROS_DISTRO --build-arg BASE_IMAGE=$BASE_IMAGE"
+#sh ./scripts/docker_build.sh ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION Dockerfile.ros.$ROS_DISTRO --build-arg BASE_IMAGE=$BASE_IMAGE
 
-sh ./scripts/docker_build.sh ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION Dockerfile.ros.$ROS_DISTRO --build-arg BASE_IMAGE=$BASE_IMAGE
+docker build -f Dockerfile.ros.$ROS_DISTRO -t ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION --build-arg BASE_IMAGE=$BASE_IMAGE .
