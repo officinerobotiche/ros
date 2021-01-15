d4eaf5173b5b111b570760c4b26f1c00f7b2dc89
Raffaello Bonghi <raffaello@rnext.it>
2021-01-15 18:47:21 +0100

    Fix variables

--- utils/variables.sh
+++ utils/variables.sh
@@ -37,7 +37,7 @@ L4T_VERSION="r32.4.4"
 OPENCV="4.5.0"
 
 # https://github.com/mdegans/nano_build_opencv
-BASE_IMAGE="$IMAGE:jp-$L4T_VERSION-cv-$OPENCV"
+BASE_IMAGE="$BASE_IMAGE_NAME:jp-$L4T_VERSION-cv-$OPENCV"
 
 TAG_IMAGE="ros:$ROS_DISTRO-ros-base-l4t-$L4T_VERSION-cv-$OPENCV"
 
